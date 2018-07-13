8cfdd2d5a667df7a33a91b7efd6b3bc622057841
Winnie Wong <winniewing55@gmail.com>
2018-07-12 19:09:04 -0700

    first commit

---
+++ js/koala.js
@@ -0,0 +1,32 @@
+//1. get the element we want (maybe document.getElementById?)
+//2. elVariableName.click
+//or addEventListener syntax (see debugging project for example)
+
+let clickable= document.querySelector("img.clickable")
+
+    clickable.addEventListener("click", clickCounter)
+
+//declare the counter variable
+
+let counter = 0
+console.log ("counter:", counter)
+
+//click handler function
+
+function clickCounter(){
+
+
+//track total clicks increment the counter variable
+
+//counter=counter+1
+  
+  counter++
+  
+//output click count to container in DOM
+console.log("counter", counter)
+  
+  // get the container, update its contents
+  
+  let container = document.qurerySelector("p#clickcount")
+  container.innerHTML=counter
+}
